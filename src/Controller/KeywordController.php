41a21c40b8ad51421551b2622a7733e7a652fd7e
Nguyen LT. Binh <binh.nguyen@nfq.asia>
2019-04-22 18:06:46 +0700

    Add upload form and action handle

--- src/Controller/KeywordController.php
+++ src/Controller/KeywordController.php
@@ -2,8 +2,10 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Form\KeywordUploaderType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class KeywordController extends AbstractController
 {
@@ -22,11 +24,38 @@ class KeywordController extends AbstractController
     /**
      * Display the form that allow user to upload CSV file
      * 
-     * @Route("/keyword/upload", name="keyword_upload")
+     * @Route("/keyword/upload", name="keyword_upload", methods={"GET"})
      */
     public function uploadAction()
     {
+        $form = $this->createForm(KeywordUploaderType::class);
         return $this->render('keyword/upload.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
+
+    /**
+     * Uploading file
+     * 
+     * @Route("/keyword/upload", name="keyword_do_upload", methods={"POST"})
+     */
+    public function doUpload(Request $request)
+    {
+        $files = $request->files->get('keyword_uploader');
+        $file = reset($files);
+
+        $fileExtension = $file->getClientOriginalExtension();
+
+        if(!in_array($fileExtension, ['csv'])) {
+            $this->addFlash('error', 'Invalid file type');
+            return $this->redirectToRoute('keyword_upload');
+        }
+
+        // Generate unique filename
+        $fileName = md5(uniqid()) . '.' . $fileExtension;
+
+        $file->move($this->getParameter('keyword_files_directory'), $fileName);
+        $this->addFlash('success', 'Great!!! Your file is being processed, please wait...');
+        return $this->redirectToRoute('keyword_upload');
+    }   
 }
